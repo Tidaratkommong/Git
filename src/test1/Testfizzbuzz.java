55ab03da8a7c35aa694a0afc7ee394821250144d
Tidaratkommong <kommong24@gmail.com>
2019-10-05 18:27:12 +0700

    input6

--- src/test1/Testfizzbuzz.java
+++ src/test1/Testfizzbuzz.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 class Testfizzbuzz1 {
 
 	@Test
-	void test5_input_5() {
+	void test6_input_6() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
-		String input = fizzbuzz.printStringinput(5);
-		assertEquals("Buzz", input);
+		String input = fizzbuzz.printStringinput(6);
+		assertEquals("Fizz", input);
 	}
 
 }
